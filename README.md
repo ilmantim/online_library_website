0ac8b51e10b9a42e6e9bf329341057614c0c85ef
ilmantim <ilmantimrod@gmail.com>
2024-03-22 14:55:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Online library website
+
+The project is a website of the online library [link](https://ilmantim.github.io/online_library_website/pages/index1.html).
+
+![Image alt](screenshot.png)
+
+## How to install
+Clone the repository to your computer.
+
+Python3 should already be installed.
+
+```
+pip install -r requirements.txt
+```
+
+## How to run a script
+
+The online library is formed on the basis of json file, which we formed earlier ([link](https://github.com/ilmantim/online_library_parser)).
+
+
+### Code Run Example
+
+````
+py render_website.py
+
+py render_website.py --page_folder pages
+
+````
+
+After the launch go to http://127.0.0.1:5500/pages/index1.html
+
+
+### Goal of the project
+Code written for educational purposes on the online course for web developers [dvmn.org](https://dvmn.org/).
---
+++ render_website.py
@@ -0,0 +1,76 @@
+import argparse
+import json
+import math
+import os
+from urllib.parse import urlsplit
+
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+from more_itertools import chunked
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Script for developing an online library'
+    )
+    parser.add_argument(
+        '-f',
+        '--file_folder',
+        help='media directory path',
+        type=str,
+        default=os.environ['LIBRARY_PATH']
+    )
+    parser.add_argument(
+        '-p',
+        '--page_folder',
+        help='path to the folder with the pages',
+        type=str,
+        default='pages/'
+    )
+    args = parser.parse_args()
+    file_folder = args.file_folder
+    page_folder = args.page_folder
+    return file_folder, page_folder
+
+
+def on_reload():
+    load_dotenv()
+    file_folder, page_folder = parse_arguments()
+    os.makedirs(page_folder, exist_ok=True)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    library_path = os.path.join(file_folder, 'library.json')
+
+    with open(library_path, "r", encoding="utf8") as my_file:
+        library = json.load(my_file)
+    for book in library:
+        image_link = urlsplit(book["image_link"])
+        book['image_link'] = image_link.path.split('/')[2]
+        book['book_path'] = os.path.join(f"{book['book_name']}.txt")
+
+    quantity_books_on_page = 10
+    columns = 2
+    pages = chunked(library, quantity_books_on_page)
+    quantity_pages = math.ceil(len(library) / quantity_books_on_page)
+    for page_number, page in enumerate(pages, 1):
+        rendered_page = template.render(library=chunked(page, columns),
+                                        quantity_pages=quantity_pages,
+                                        page_number=page_number)
+        page_path = os.path.join(page_folder, f'index{page_number}.html')
+        with open(page_path, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+
+def main():
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+
+
+if __name__ == '__main__':
+    main()
